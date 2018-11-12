5ce6c8e20ca0bba8a889dd4d2af38c25d8df309d
faissaloo <faissaloo@gmail.com>
2018-11-12 21:39:35 +0000

    Made language model generator more appropriate for livespeech

--- generate_language_model.sh
+++ generate_language_model.sh
@@ -4,5 +4,5 @@ sort -u -f transcripts.txt -o transcripts.txt
 grep -o -E '\w+' transcripts.txt | sort -u -f > lm/words.txt
 grep -o -E '.' transcripts.txt | sort -u -f > lm/alphabet.txt
 lmplz -o 2 < transcripts.txt > lm/transcripts.arpa
-build_binary trie lm/transcripts.arpa lm/transcripts.binary -q 8 -a 255
-../DeepSpeech/generate_trie lm/alphabet.txt lm/transcripts.binary lm/words.txt lm/vocab.trie
+build_binary trie lm/transcripts.arpa lm/lm.binary -q 8 -a 255
+../DeepSpeech/generate_trie lm/alphabet.txt lm/lm.binary lm/words.txt lm/trie
